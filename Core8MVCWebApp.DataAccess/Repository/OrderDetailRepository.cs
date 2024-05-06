e8a10c37d5251f5fa98c06a09ef78350e0d8643c
Tijo Jacob <tijojosejacob@gmail.com>
2024-05-06 15:49:16 +0530

    Order Header&Details Repository-Interface with UnitOfWork

---
+++ Core8MVCWebApp.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,30 @@
+﻿using Core8MVC.DataAccess.Repository.IRepository;
+using Core8MVC.Models.Models;
+using Core8MVCWebApp.Controllers.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core8MVC.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        //public void Save()
+        //{
+        //    _db.SaveChanges();
+        //}
+
+        public void Update(OrderDetail orderDetail)
+        {
+            _db.OrderDetails.Update(orderDetail);
+        }
+    }
+}
---
+++ Core8MVCWebApp.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,30 @@
+﻿using Core8MVC.DataAccess.Repository.IRepository;
+using Core8MVC.Models.Models;
+using Core8MVCWebApp.Controllers.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core8MVC.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        //public void Save()
+        //{
+        //    _db.SaveChanges();
+        //}
+
+        public void Update(OrderHeader orderHeader)
+        {
+            _db.OrderHeaders.Update(orderHeader);
+        }
+    }
+}
